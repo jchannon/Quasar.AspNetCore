13150a738db236ce3c89366bb563d45508aa75f7
George Kinsman <george@georgekinsman.com>
2023-09-20 19:37:51 +0100

    Always use same port so it matches .net

--- Quasar.AspNetCore/app/quasar.config.js
+++ Quasar.AspNetCore/app/quasar.config.js
@@ -97,6 +97,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      port: 9000,
       open: true // opens browser window automatically
     },
 
