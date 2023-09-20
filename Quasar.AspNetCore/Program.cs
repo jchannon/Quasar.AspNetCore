5e2188fe5cf8068e830de8165cc8301b412b3190
George Kinsman <george@georgekinsman.com>
2023-09-20 18:41:34 +0100

    Add vite middleware to forward dev server
    - quasar app not loading in dev for some reason (even through vite directly)
    - remove build assets

--- Quasar.AspNetCore/Program.cs
+++ Quasar.AspNetCore/Program.cs
@@ -1,7 +1,27 @@
+using Vite.AspNetCore;
+using Vite.AspNetCore.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddViteServices(new ViteOptions()
+{
+    Server = new ViteServerOptions()
+    {
+        Port = 4000
+    }
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    // Enable the Middleware to use the Vite Development Server.
+    app.UseViteDevMiddleware();
+}
 
 app.UseStaticFiles();
 app.MapGet("/qwerty", () => "Hello World!");
+
+
 
 app.Run();
